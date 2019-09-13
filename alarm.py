b4fedd249d95c43e50a3fe97f4fcde69bfc10124
Jeremias Pretto <jerepretto@gmail.com>
2019-09-13 01:42:01 -0300

    .

---
+++ alarm.py
@@ -0,0 +1,16 @@
+import yaml
+import os
+import requests
+from bs4 import BeautifulSoup
+
+base_path = os.path.dirname(os.path.abspath(__file__))
+config_file = 'config.yml'
+
+with open(os.path.join(base_path, config_file), 'r') as ymlfile:
+    cfg = yaml.safe_load(ymlfile)
+
+ctfs = cfg["ctfds"]
+for ctf in ctfs:
+    host = ctfs[ctf]["host"]
+    user = ctfs[ctf]["user"]
+    paswd = ctfs[ctf]["paswd"]
